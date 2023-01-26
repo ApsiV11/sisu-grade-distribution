4b87e0db2a86ae33798903493a37b9228aa77714
Aapo Laakkio <aapolaakkio@outlook.com>
2023-01-27 00:42:36 +0200

    Add distributions to failed/expired page

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(changeInfo.status === 'complete' && tab.url.indexOf("valid-attainments")!=-1) {
+    if(changeInfo.status === 'complete' && (tab.url.indexOf("valid-attainments")!=-1 || tab.url.indexOf("failed-and-expired-attainments")!=-1)) {
         chrome.scripting.executeScript({
             target : {tabId : tabId},
             files : ["scripts/inject_chart.js", "scripts/chart.umd.js"],
