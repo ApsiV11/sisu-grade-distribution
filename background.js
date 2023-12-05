a3a4f18fabada770b20f51bd13a8282f48e6f54b
Aapo Laakkio <aapolaakkio@outlook.com>
2023-12-05 21:07:52 +0200

    Remove injected checking since it seems to be working better without it

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-let injected = false
-
 const urls = [
     "https://sisu.aalto.fi",
     "https://sisu.helsinki.fi",
@@ -12,17 +10,11 @@ const urls = [
 ]
 
 function listener(tabId, changeInfo, tab){
-    let inSisuPage = false
-    for(let url of urls){
-        if(tab.url.startsWith(url)) inSisuPage = true
-    }
-    if(!inSisuPage) injected = false
-    if(!injected && changeInfo.status === 'complete' && (tab.url.indexOf("valid-attainments")!=-1 || tab.url.indexOf("failed-and-expired-attainments")!=-1)) {
+    if(changeInfo.status === 'complete' && (tab.url.indexOf("valid-attainments")!=-1 || tab.url.indexOf("failed-and-expired-attainments")!=-1)) {
         chrome.scripting.executeScript({
             target : {tabId : tabId},
             files : ["scripts/inject_chart.js", "scripts/chart.umd.js"],
         })
-        injected = true
     }
 }
 
