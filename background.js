6522027479e81be745ec333bf7c8339f47059ffe
Aapo Laakkio <aapolaakkio@outlook.com>
2025-01-06 15:41:16 +0200

    Fix changed url path

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ const urls = [
 ]
 
 function listener(tabId, changeInfo, tab){
-    if(changeInfo.status === 'complete' && (tab.url.indexOf("valid-attainments")!=-1 || tab.url.indexOf("failed-and-expired-attainments")!=-1)) {
+    if(changeInfo.status === 'complete' && (tab.url.indexOf("profile/attainments")!=-1)) {
         chrome.scripting.executeScript({
             target : {tabId : tabId},
             files : ["scripts/inject_chart.js", "scripts/chart.umd.js"],
